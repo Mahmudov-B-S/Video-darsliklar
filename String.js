36bb3207b5b7d55d1fe34282b191f95210a85a24
Mahmudov Bekmuhammad <work.frontend7@gmail.com>
2023-02-12 21:34:40 +0500

    String

---
+++ String.js
@@ -0,0 +1,34 @@
+// let a = "  hello world"
+// let b = "123,456,789"
+
+// console.log(String.row`"aasdf
+// asd
+// asdf
+// asdf
+// asdfghgfd
+// sdfghggfdfsdfdgf
+// dsfdgfhgfdsf"`
+// );
+
+// console.log(a.concat(b)); // concat()
+
+// console.log(b.split(",")); // split()
+// console.log(a.split("").length);
+// console.log(a.split().length);
+
+// console.log(a.trimEnd()); // trimEnd()
+// console.log(a.trimStart()); // trimStart()
+
+// console.log(a.length); // trim()
+// console.log(a.trim().length);
+
+// console.log(eval("1 + 2 + 3 + 4s")); // eval()
+
+// console.log(a.substring(3, 8)); // substring()
+
+// console.log(b.slice(3, 8)); // slice()
+
+// console.log(b.padStart(13, "+998")); // padStart()
+
+// console.log(a.padEnd(10 , "world")); // padEnd()
+
