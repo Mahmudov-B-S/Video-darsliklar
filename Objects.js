3311734417093fb5569ced38b8216098c41b8be0
Mahmudov Bekmuhammad <work.frontend7@gmail.com>
2023-02-14 22:32:57 +0500

    Objects

---
+++ Objects.js
@@ -0,0 +1,27 @@
+// let webbrain = {
+//     title: "webbrain academy",
+//     year: 2019,
+//     location: "chilonzor",
+//     "full name": "wba"
+// }
+
+
+// console.log(webbrain["full name"]); // -- wba
+
+
+// console.log(webbrain.title); //  -- webbrain academy
+// console.log(webbrain.year); //  -- 2019
+// console.log(webbrain.location); //  -- chilonzor
+
+
+// let title = "webbrain academy"
+// let year = 2019
+// let location = "chilonzor"
+
+// let titleDig = "Digital one"
+// let yearDig = 2020
+// let locationDig = "Beruniy"
+
+
+// console.log(title, year, location);
+
