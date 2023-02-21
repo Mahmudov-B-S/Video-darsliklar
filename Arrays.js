71813a998856aab301d879f40332be6df9852b76
Mahmudov Bekmuhammad <work.frontend7@gmail.com>
2023-02-21 23:52:45 +0500

    Advenced arrays

---
+++ Arrays.js
@@ -0,0 +1,40 @@
+// Arrays
+
+// let ar = [1, "afs", {dfgh: "dgdfyg"}, ["dfgh", "dfgh"]]
+
+// console.log(ar.length);
+
+// let num1 = [1, 2, 3, 4]
+// let num2 = [5, 6, 7, 8]
+
+// console.log(num1.concat(num2));
+
+// let ar = ["name", "asad", "age", "19"]
+// ar.push("surname")
+// ar.unshift("surname")
+// ar.shift()
+// ar.pow()
+// console.log(ar.pow());
+
+
+
+// let ar1 = [["mane", "Bek"], ["surname", "Mahmudov"]]
+
+
+
+// console.log(ar.concat(ar1));
+
+// console.log(ar + ar1);
+
+
+// ar2.newArray [12, 234, 345, "dgsds"]
+
+// console.log(ar2);
+
+// console.log(Object.fromEntries(ar1));
+
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+
+// console.log(Object.entries(obj));
