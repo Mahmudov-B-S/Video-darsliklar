468d27b3ba39ceffa5aad3be646bec1db72523fd
Mahmudov Bekmuhammad <work.frontend7@gmail.com>
2023-03-04 23:00:38 +0500

    Rekrusiya

---
+++ Crud.js
@@ -0,0 +1,11 @@
+let users = [
+    { id:1, name:"qwerty"},
+    { id:2, name:"sdfgh"},
+    { id:3, name:"xcvbn"},
+    { id:4, name:"wertyui"},
+    { id:5, name:"lkjhg"}
+]
+
+// const filterStd = ( )
+
+console.log(users);
---
+++ Recursion.js
@@ -0,0 +1,10 @@
+// const sum = (n) => {
+//     let s = 0;
+//     for (let i = 1; i <= n; i++) {
+//         s += i
+//     }
+//     console.log(s);
+// }
+// sum(5)
+
+
